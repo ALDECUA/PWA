beb26b3e7304444dfc5a866013898511d7aed30c
ALDECUA <cruz.aldej@gmail.com>
2021-11-29 00:25:21 -0600

    .

--- script.js
+++ script.js
@@ -5,91 +5,62 @@ if ('serviceWorker' in navigator) {
   }
 
 
-function cambio1() {
-  document.getElementById('año1').innerHTML="1";
-  document.getElementById('año2').innerHTML="1 mdp";
-  document.getElementById('año3').innerHTML="1";
-  document.getElementById('año4').innerHTML="1";
-  document.getElementById('año5').innerHTML="1 mdp";
-  document.getElementById('año6').innerHTML="1";
-  document.getElementById('a').style.backgroundColor='red';
-  document.getElementById('b').style.backgroundColor='transparent';
-  document.getElementById('c').style.backgroundColor='transparent';
-  document.getElementById('d').style.backgroundColor='transparent';
-  document.getElementById('e').style.backgroundColor='transparent';
-  document.getElementById('f').style.backgroundColor='transparent';
-}
+
   
  function cambio2() {
-  document.getElementById('año1').innerHTML="2";
-  document.getElementById('año2').innerHTML="2 mdp";
-  document.getElementById('año3').innerHTML="2";
-  document.getElementById('año4').innerHTML="2";
-  document.getElementById('año5').innerHTML="2 mdp";
-  document.getElementById('año6').innerHTML="2";
-  document.getElementById('a').style.backgroundColor='transparent';
+  document.getElementById('año1').innerHTML="1729";
+  document.getElementById('año2').innerHTML="21 MP";
+  document.getElementById('año3').innerHTML="1125";
+  document.getElementById('año4').innerHTML="6156";
+  document.getElementById('año5').innerHTML="17 MP";
+  document.getElementById('año6').innerHTML="5732";
   document.getElementById('b').style.backgroundColor='red';
   document.getElementById('c').style.backgroundColor='transparent';
   document.getElementById('d').style.backgroundColor='transparent';
   document.getElementById('e').style.backgroundColor='transparent';
-  document.getElementById('f').style.backgroundColor='transparent';
 }
+
 function cambio3() {
-  document.getElementById('año1').innerHTML="3";
-  document.getElementById('año2').innerHTML="3 mdp";
-  document.getElementById('año3').innerHTML="3";
-  document.getElementById('año4').innerHTML="3";
-  document.getElementById('año5').innerHTML="3 mdp";
-  document.getElementById('año6').innerHTML="3";
-  document.getElementById('a').style.backgroundColor='transparent';
+  document.getElementById('año1').innerHTML="613";
+  document.getElementById('año2').innerHTML="6 MP";
+  document.getElementById('año3').innerHTML="290";
+  document.getElementById('año4').innerHTML="2215"
+  document.getElementById('año5').innerHTML="5 MP";
+  document.getElementById('año6').innerHTML="2035";
   document.getElementById('b').style.backgroundColor='transparent';
   document.getElementById('c').style.backgroundColor='red';
   document.getElementById('d').style.backgroundColor='transparent';
   document.getElementById('e').style.backgroundColor='transparent';
-  document.getElementById('f').style.backgroundColor='transparent';
+  
 }
 function cambio4() {
-  document.getElementById('año1').innerHTML="4";
-  document.getElementById('año2').innerHTML="4 mdp";
-  document.getElementById('año3').innerHTML="4 ";
-  document.getElementById('año4').innerHTML="4";
-  document.getElementById('año5').innerHTML="4 mdp";
-  document.getElementById('año6').innerHTML="4";
-  document.getElementById('a').style.backgroundColor='transparent';
+  document.getElementById('año1').innerHTML="747";
+  document.getElementById('año2').innerHTML="7 MP";
+  document.getElementById('año3').innerHTML="258 ";
+  document.getElementById('año4').innerHTML="1967";
+  document.getElementById('año5').innerHTML="7 MP";
+  document.getElementById('año6').innerHTML="1776";
+  
   document.getElementById('b').style.backgroundColor='transparent';
   document.getElementById('c').style.backgroundColor='transparent';
   document.getElementById('d').style.backgroundColor='red';
   document.getElementById('e').style.backgroundColor='transparent';
-  document.getElementById('f').style.backgroundColor='transparent';
 }
 function cambio5() {
-  document.getElementById('año1').innerHTML="3,089";
-  document.getElementById('año2').innerHTML="35 mdp";
-  document.getElementById('año3').innerHTML="1,671";
-  document.getElementById('año4').innerHTML="13,332";
-  document.getElementById('año5').innerHTML="40.6 mdp";
-  document.getElementById('año6').innerHTML="12,265";
-  document.getElementById('a').style.backgroundColor='transparent';
+  document.getElementById('año1').innerHTML="3089";
+  document.getElementById('año2').innerHTML="35 MP";
+  document.getElementById('año3').innerHTML="1671";
+  document.getElementById('año4').innerHTML="13332";
+  document.getElementById('año5').innerHTML="40.6 MP";
+  document.getElementById('año6').innerHTML="12265";
+  
   document.getElementById('b').style.backgroundColor='transparent';
   document.getElementById('c').style.backgroundColor='transparent';
   document.getElementById('d').style.backgroundColor='transparent';
   document.getElementById('e').style.backgroundColor='red';
-  document.getElementById('f').style.backgroundColor='transparent';
+  
 }
-function cambio6() {
-  document.getElementById('año1').innerHTML="6";
-  document.getElementById('año2').innerHTML="6 mdp";
-  document.getElementById('año3').innerHTML="6";
-  document.getElementById('año4').innerHTML="6";
-  document.getElementById('año5').innerHTML="6 mdp";
-  document.getElementById('año6').innerHTML="6";
-  document.getElementById('a').style.backgroundColor='transparent';
-  document.getElementById('b').style.backgroundColor='transparent';
-  document.getElementById('c').style.backgroundColor='transparent';
-  document.getElementById('d').style.backgroundColor='transparent';
-  document.getElementById('e').style.backgroundColor='transparent';
-  document.getElementById('f').style.backgroundColor='red';
-}
+
 
 
 
